e6a6ff82dedfc0dd1491f3326fdcf63f64da568f
shjwudp <shjwudp@gmail.com>
2021-06-29 16:08:54 +0800

    ..

--- bagua-core-internal/src/env_var.rs
+++ bagua-core-internal/src/env_var.rs
@@ -1,3 +1,6 @@
 pub fn get_rank() -> i32 {
-    return std::env::var("RANK").unwrap_or("0".to_string()).parse::<i32>();
+    return std::env::var("RANK")
+        .unwrap_or("0".to_string())
+        .parse::<i32>()
+        .unwrap();
 }
